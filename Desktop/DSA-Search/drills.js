1c0d63ea2131a570286227c0f512ede0e2959c6b
stephenschab <sschab689@gmail.com>
2020-01-29 12:19:47 -0700

    added linear button functionality

--- Desktop/DSA-Search/drills.js
+++ Desktop/DSA-Search/drills.js
@@ -24,41 +24,37 @@ function binarySearch(array, value, start, end) {
 
 ////////////////////////////
 
-function _printArray(arr) {
-  let newArr = [];
-  for (let i = 0; i < arr.length; i++) {
-    newArr.push(arr[i]);
-  }
-  return newArr;
-}
-
+let sequenceOfSearch = [];
 function howManySearches(array, value, start, end) {
   let beginning = start === undefined ? 0 : start;
   let arrEnd = end === undefined ? array.length : end;
-
+  // console.log(_printArray(array));
   if (beginning > arrEnd) {
-    return 'number not found';
+    return 'value not found';
   }
-
   const index = Math.floor((beginning + arrEnd) / 2);
   const item = array[index];
   // console.log(beginning, arrEnd);
-  console.log(_printArray(array));
-
-
+  console.log(sequenceOfSearch);
   if (item === value) {
     return `Found item: ${item}`;
   }
   else if (item < value) {
+    sequenceOfSearch.push(item);
     return howManySearches(array, value, index + 1, end);
   }
   else if (item > value) {
-
+    sequenceOfSearch.push(item);
     return howManySearches(array, value, start, index - 1);
   }
-
 }
 let sortedList = [3, 5, 6, 8, 11, 12, 14, 15, 17, 18];
-
 // console.log(howManySearches(sortedList, 8));
-console.log(howManySearches(sortedList, 16));
+// [ 12 ]
+// [ 12, 6 ]
+// Found item: 8
+// console.log(howManySearches(sortedList, 16));
+// [ 12 ]
+// [ 12, 17 ]
+// [ 12, 17, 14 ]
+// value not found
--- Desktop/DSA-Search/dsa-react-ui/src/App.js
+++ Desktop/DSA-Search/dsa-react-ui/src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    dataSet: [89, 30, 25, 32, 72, 70, 51, 42, 25, 24, 53, 55, 78, 50, 13, 40, 48, 32, 26, 2, 14, 33, 45, 72, 56, 44, 21, 88, 27, 68, 15, 62, 93, 98, 73, 28, 16, 46, 87, 28, 65, 38, 67, 16, 85, 63, 23, 69, 64, 91, 9, 70, 81, 27, 97, 82, 6, 88, 3, 7, 46, 13, 11, 64, 76, 31, 26, 38, 28, 13, 17, 69, 90, 1, 6, 7, 64, 43, 9, 73, 80, 98, 46, 27, 22, 87, 49, 83, 6, 39, 42, 51, 54, 84, 34, 53, 78, 40, 14, 5],
+    input: 'Enter number here',
+    output: null
+  }
+
+  linearSearch = (arr, input) => {
+    console.log(arr, input);
+    let counter = 0;
+    for (let i =0; i < arr.length; i++) {
+      counter++;
+      if (arr[i] === parseInt(input)) {
+        return `${input} found! it took ${counter} attempts`;
+      }
+    }
+    return `No matching value, took ${counter} attempts`
+  }
+  
+
+  handleLinear = (event) => {
+    event.preventDefault();
+    const newOutput = this.linearSearch(this.state.dataSet, this.state.input);
+    this.setState({
+      output: newOutput,
+      input: 'Enter number here'
+    })
+  }
+
+  handleInput = (event) => {
+    this.setState({
+      input: event.target.value
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>React UI Drill</h1>
+        <form>
+          <label htmlFor='input'></label>
+          <input type='text' id='input' value={this.state.input} onChange={this.handleInput}/>
+          <button onClick={ev => this.handleLinear(ev)}>Linear</button>
+          <button onClick={ev => this.handleBinary(ev)}>Binary</button>
+        </form>
+        <div className='output'>
+          <p>{this.state.output}</p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
