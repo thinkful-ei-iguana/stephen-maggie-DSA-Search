53ec518d4b6fbd6b559c78edc22d294983bf965c
therealmbittarelli <maggiekmcclellan@gmail.com>
2020-01-29 15:23:40 -0500

    finishing ex 4

--- Desktop/DSA-Search/drills.js
+++ Desktop/DSA-Search/drills.js
@@ -58,3 +58,56 @@ let sortedList = [3, 5, 6, 8, 11, 12, 14, 15, 17, 18];
 // [ 12, 17 ]
 // [ 12, 17, 14 ]
 // value not found
+
+
+// 2. See dsa-react-ui
+
+// 3. Find a book
+
+// this is a binary search, modified to work with the Dewey Decimal System
+function findBook(library, dewey, title, start, end) {
+  let beginning = start === undefined ? 0 : start;
+  let arrEnd = end === undefined ? library.length : end;
+
+  if (beginning > arrEnd) {
+    return 'Book not found';
+  }
+
+  const index = Math.floor((beginning + arrEnd) / 2);
+  const item = library[index];
+
+  console.log(beginning, arrEnd);
+
+  if (item === library) {
+    return `${item.value} with Dewey Decimal ${item.key}`;
+  }
+  else if (item < dewey) {
+    return findBook(library, dewey, index + 1, end);
+  }
+  else if (item > dewey) {
+    return findBook(library, dewey, start, index - 1);
+  }
+}
+
+// 4.
+
+        //        35
+        //        / \
+        //      25   89
+        //     / \  /  \
+        //   15  27 79  91
+        //  /  \        /
+        // 14  19      90
+
+  // 1) post order traversal: 14, 19, 15, 27, 25, 79, 90, 91, 89, 35
+
+        //       8
+        //     /   \
+        //    6     10
+        //   / \    / \
+        //  5   7  9   11
+
+  // 2) pre order traversal: 8, 6, 5, 7, 10, 9, 11
+
+
+// 5.
--- Desktop/DSA-Search/dsa-react-ui/src/App.js
+++ Desktop/DSA-Search/dsa-react-ui/src/App.js
@@ -2,17 +2,19 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
   state = {
     dataSet: [89, 30, 25, 32, 72, 70, 51, 42, 25, 24, 53, 55, 78, 50, 13, 40, 48, 32, 26, 2, 14, 33, 45, 72, 56, 44, 21, 88, 27, 68, 15, 62, 93, 98, 73, 28, 16, 46, 87, 28, 65, 38, 67, 16, 85, 63, 23, 69, 64, 91, 9, 70, 81, 27, 97, 82, 6, 88, 3, 7, 46, 13, 11, 64, 76, 31, 26, 38, 28, 13, 17, 69, 90, 1, 6, 7, 64, 43, 9, 73, 80, 98, 46, 27, 22, 87, 49, 83, 6, 39, 42, 51, 54, 84, 34, 53, 78, 40, 14, 5],
-    input: 'Enter number here',
-    output: null
+    input: '',
+    output: null,
+    counter: 0
   }
 
   linearSearch = (arr, input) => {
     console.log(arr, input);
     let counter = 0;
-    for (let i =0; i < arr.length; i++) {
+    for (let i = 0; i < arr.length; i++) {
       counter++;
       if (arr[i] === parseInt(input)) {
         return `${input} found! it took ${counter} attempts`;
@@ -20,14 +22,63 @@ class App extends Component {
     }
     return `No matching value, took ${counter} attempts`
   }
-  
+
+  binarySearch = (array, value, start, end) => {
+    let beginning = start === undefined ? 0 : start;
+    let arrEnd = end === undefined ? array.length : end;
+
+
+
+    if (beginning > arrEnd) {
+      return `No matching value, took ${this.state.counter} attempts`;
+    }
+
+    const index = Math.floor((beginning + arrEnd) / 2);
+    const item = array[index];
+
+    console.log(beginning, arrEnd);
+    if (item == value) {
+      this.setState({
+        counter: this.state.counter++
+      })
+      console.log('found item counter is', this.state.counter);
+      return `${item} found! it took ${this.state.counter} attempts`;
+    }
+    else if (item < value) {
+      this.setState({
+        counter: this.state.counter++
+      })
+      console.log('item less than value counter is', this.state.counter);
+      return this.binarySearch(array, value, index + 1, arrEnd);
+    }
+    else if (item > value) {
+      this.setState({
+        counter: this.state.counter++
+      })
+      console.log('item greater than value counter is', this.state.counter);
+      return this.binarySearch(array, value, beginning, index - 1);
+    }
+  }
+
 
   handleLinear = (event) => {
     event.preventDefault();
     const newOutput = this.linearSearch(this.state.dataSet, this.state.input);
     this.setState({
       output: newOutput,
-      input: 'Enter number here'
+      input: ''
+    })
+  }
+
+  handleBinary = (event) => {
+    event.preventDefault();
+    const sortedArr = this.state.dataSet.sort((a, b) => {
+      return a - b
+    })
+    const newOutput = this.binarySearch(sortedArr, this.state.input);
+    this.setState({
+      output: newOutput,
+      input: ''
     })
   }
 
@@ -42,7 +93,7 @@ class App extends Component {
         <h1>React UI Drill</h1>
         <form>
           <label htmlFor='input'></label>
-          <input type='text' id='input' value={this.state.input} onChange={this.handleInput}/>
+          <input type='text' id='input' value={this.state.input} onChange={this.handleInput} />
           <button onClick={ev => this.handleLinear(ev)}>Linear</button>
           <button onClick={ev => this.handleBinary(ev)}>Binary</button>
         </form>
